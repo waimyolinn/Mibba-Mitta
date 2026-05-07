a533a071becf75a89086df01f214c32cca799790
waimyolinn <waimyolinnaoehtein@gmail.com>
2026-05-07 14:16:29 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mibamittaa-v7';
+const CACHE_NAME = 'mibamittaa-v8';
 const API_CACHE = 'api-cache-v1';
 
 self.addEventListener('install', event => {
@@ -14,7 +14,12 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
     if (url.hostname.includes('workers.dev')) {
-        event.respondWith(caches.open(API_CACHE).then(cache => fetch(event.request).then(res => { if (res.ok) cache.put(event.request, res.clone()); return res; }).catch(() => cache.match(event.request))));
+        event.respondWith(caches.open(API_CACHE).then(cache => 
+            fetch(event.request).then(res => { 
+                if (res.ok) cache.put(event.request, res.clone()); 
+                return res; 
+            }).catch(() => cache.match(event.request))
+        ));
         return;
     }
     event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
