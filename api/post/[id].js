7f3787ef0b089acc09c2706925e3019aed2b3958
waimyolinn <waimyolinnaoehtein@gmail.com>
2026-05-15 21:00:16 +0630

    Update [id].js

--- api/post/[id].js
+++ api/post/[id].js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // Force JSON response
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'DELETE, OPTIONS');
@@ -9,11 +8,10 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
   
-  // Always return success for testing
   return res.status(200).json({ 
     success: true, 
     method: req.method,
     id: req.query.id,
-    token_received: !!req.headers.authorization
+    token: req.headers.authorization ? 'yes' : 'no'
   });
 }
